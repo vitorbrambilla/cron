ad7bbf6285efe5cf59b91e213bbda057a492a544
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-08-16 17:14:25 -0300

    first commit

---
+++ cron-delete.js
@@ -0,0 +1,14 @@
+const cron = require('node-cron');
+const fs = require('fs');
+
+// ...
+
+// Remove the error.log file every twenty-first day of the month.
+cron.schedule('* * * * *', function() {
+    console.log('---------------------');
+    console.log('Running Cron Job');
+    fs.unlink('./error.log', err => {
+      if (err) throw err;
+      console.log('Error file successfully deleted');
+    });
+  });
